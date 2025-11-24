ad1682ea7c2183b65d53735c715fde132e06eb57
dominiksujeta97-collab <dominik.sujeta97@gmail.com>
2025-11-24 22:41:42 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -12,10 +12,34 @@ export function main(dtoIn) {
     let ageMin = dtoIn.age.min;
     let ageMax = dtoIn.age.max;
 
-    let maleNames = ["Peter", "Martin", "Jakub", "Samuel", "Lukáš", "Michal", "Adam", "Tomáš", "Matej", "Dominik"];
-    let femaleNames = ["Lucia", "Kristína", "Natália", "Ema", "Sofia", "Laura", "Monika", "Zuzana", "Veronika", "Katarína"];
-    let maleSurnames = ["Novák", "Kováč", "Horváth", "Varga", "Tóth", "Kučera", "Marek", "Bartoš", "Urban", "Šimek"];
-    let femaleSurnames = ["Nováková", "Kováčová", "Horváthová", "Vargová", "Tóthová", "Kučerová", "Marková", "Bartošová", "Urbanová", "Šimková"];
+    let maleNames = [
+        "Peter", "Martin", "Jakub", "Samuel", "Lukáš", "Michal", "Adam", "Tomáš", "Matej", "Dominik",
+        "Filip", "Patrik", "Andrej", "Daniel", "Erik", "Oliver", "Marek", "Sebastián", "Viktor", "Roman",
+        "Rastislav", "Boris", "Ján", "Šimon", "Dávid", "Karol", "Igor", "Norbert", "Gabriel", "Henrich",
+        "Marcel", "Alan", "Teodor", "Richard", "Leonard", "Juraj", "Pavol", "Robo", "Miloš", "Tibor",
+        "Oliver", "Eduard", "Branislav", "Radovan", "Metod", "Tadeáš", "Benedikt", "Frederik", "Kornel", "Vojtech"
+    ];
+    let femaleNames = [
+        "Lucia", "Kristína", "Natália", "Ema", "Sofia", "Laura", "Monika", "Zuzana", "Veronika", "Katarína",
+        "Eva", "Mária", "Barbora", "Petra", "Simona", "Nikola", "Tamara", "Viktória", "Paulína", "Lenka",
+        "Jana", "Diana", "Ela", "Nina", "Bianka", "Tatiana", "Andrea", "Gabriela", "Alžbeta", "Tereza",
+        "Sara", "Vanesa", "Stela", "Renáta", "Sabina", "Marína", "Bohdana", "Timea", "Rebeka", "Júlia",
+        "Karolína", "Kristiána", "Linda", "Romana", "Mia", "Dominika", "Helena", "Adriana", "Ružena", "Beáta"
+    ];
+    let maleSurnames = [
+        "Novák", "Kováč", "Horváth", "Varga", "Tóth", "Kučera", "Marek", "Bartoš", "Urban", "Šimek",
+        "Král", "Klement", "Farkaš", "Klein", "Hruška", "Sokol", "Baran", "Roth", "Hlaváč", "Polák",
+        "Ford", "Keller", "Berger", "Černý", "Bielik", "Potočný", "Mašek", "Dostál", "Veselý", "Hlavatý",
+        "Kráľ", "Červeň", "Dvořák", "Kubík", "Roth", "Benko", "Hrdlička", "Zima", "Ševčík",
+        "Hájek", "Holub", "Štěpánek", "Kolár", "Bernard", "Gregor", "Kovář", "Majer", "Ondrej", "Kadlec"
+    ];
+    let femaleSurnames = [
+        "Nováková", "Kováčová", "Horváthová", "Vargová", "Tóthová", "Kučerová", "Marková", "Bartošová", "Urbanová", "Šimková",
+        "Králová", "Klementová", "Farkašová", "Kleinová", "Hrušková", "Sokolová", "Baranová", "Rothová", "Hlaváčová", "Poláková",
+        "Fordová", "Kellerová", "Bergerová", "Černá", "Bieliková", "Potočná", "Mašková", "Dostálová", "Veselá", "Hlavatá",
+        "Kráľová", "Červeňová", "Dvořáková", "Kubíková", "Benkoová", "Hrdličková", "Zimová", "Ševčíková",
+        "Hájková", "Holubová", "Štěpánková", "Kolárová", "Bernardová", "Gregorová", "Kovářová", "Majerová", "Ondrejová", "Kadlecová"
+    ];
     let workloads = [10, 20, 30, 40];
 
     function pickRandom(list) {
