ec591196f22f2b9fc1bd5ab60a93e2005d1abc25
dominiksujeta97-collab <dominik.sujeta97@gmail.com>
2025-11-25 19:38:51 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -6,12 +6,18 @@
  * @param {object} dtoIn contains count of employees, age limit of employees {min, max}
  * @returns {Array} dtoOut - list of employees
  */
+
+//Hlavna funkcia na vytvorenie zoznamu zamestnancov
 export function main(dtoIn) {
 
+    //Pocet zamestnancov, ktory vytvarame
     let count = dtoIn.count;
+
+    //Minimalny a maximalny vek
     let ageMin = dtoIn.age.min;
     let ageMax = dtoIn.age.max;
 
+    //Zoznam nahodnych mien
     let names = [
         "Peter", "Martin", "Jakub", "Samuel", "Lukas", "Michal", "Adam", "Tomas", "Matej", "Dominik",
         "Filip", "Patrik", "Andrej", "Daniel", "Erik", "Oliver", "Marek", "Sebastian", "Viktor", "Roman",
@@ -20,6 +26,7 @@ export function main(dtoIn) {
         "Eva", "Maria", "Barbora", "Petra", "Simona", "Nikola", "Tamara", "Viktoria", "Paulina", "Lenka"
     ];
 
+    //Zoznam nahodnych priezvisk
     let surnames = [
         "Novak", "Kovac", "Horvath", "Varga", "Toth", "Kucera", "Marek", "Bartok", "Urban", "Simek",
         "Kral", "Klement", "Farkas", "Klein", "Hruska", "Sokol", "Baran", "Roth", "Hlavac", "Polak",
@@ -30,36 +37,42 @@ export function main(dtoIn) {
         "Sokolova", "Baranova", "Rothova", "Hlavacova", "Polakova"
     ];
 
+    //Mozny pracovny uvazok
     let workloads = [10, 20, 30, 40];
 
+    //Funkcia pre nahodny vyber z pola
     function pickRandom(list) {
         let index = Math.floor(Math.random() * list.length);
         return list[index];
     }
 
+    //Set na uz pouzite datumy narodenia - zabezpecenie jedinecnosti datumu
     let usedBirthdates = new Set();
 
+    //Funkcia na vypocet veku podla datumu s premenou na milisekundy - testy mi zial padali pri pocitani kalendarnych rokov, preto praca s milisekundami
     function getAgeFromDate(date) {
         let diffMs = Date.now() - date.getTime();
         let years = diffMs / (365.25 * 24 * 60 * 60 * 1000);
         return years;
     }
 
+    //Funkcia na generaciu nahodneho datumu narodenia pri opakovani, ak uz bol datum pouzity
+    //Prevod do ISO formatu
     function generateBirthdate(minAge, maxAge) {
         while (true) {
             let age = minAge + Math.random() * (maxAge - minAge);
 
             let diffMs = age * 365.25 * 24 * 60 * 60 * 1000;
-            let d = new Date(Date.now() - diffMs);
-            d.setUTCHours(0, 0, 0, 0);
+            let birthday = new Date(Date.now() - diffMs);
+            birthday.setUTCHours(0, 0, 0, 0);
 
-            let iso = d.toISOString();
+            let iso = birthday.toISOString();
 
             if (usedBirthdates.has(iso)) {
                 continue;
             }
-
-            let realAge = getAgeFromDate(d);
+            //Overenie veku
+            let realAge = getAgeFromDate(birthday);
             if (realAge > minAge && realAge < maxAge) {
                 usedBirthdates.add(iso);
                 return iso;
@@ -67,16 +80,25 @@ export function main(dtoIn) {
         }
     }
 
+    //Vystup
     let dtoOut = [];
 
+    //Cyklus na vytvorenie nahodnych zamestnancov
     for (let i = 0; i < count; i++) {
 
-        let gender = Math.random() < 0.5 ? "male" : "female";
+        let gender;
+        if (Math.random() < 0.5) {
+            gender = "male";
+        } else {
+            gender = "female";
+        }
+
         let name = pickRandom(names);
         let surname = pickRandom(surnames);
         let birthdate = generateBirthdate(ageMin, ageMax);
         let workload = pickRandom(workloads);
 
+        //Vytvorenie zamestnanaca
         let employee = {
             gender: gender,
             birthdate: birthdate,
@@ -85,7 +107,7 @@ export function main(dtoIn) {
             workload: workload
         };
 
-        dtoOut.push(employee);
+        dtoOut.push(employee); //Pridame do generovaneho vystupneho zoznamu
     }
 
     return dtoOut;
